45de13498bb8465e109858dccd17f3042aa90df2
King Villanueva <parvillanueva@gmail.com>
2020-02-05 08:43:31 +0800

    add jqeury cdn

--- application/views/site/layout/header.php
+++ application/views/site/layout/header.php
@@ -44,6 +44,8 @@
 
         ?>
 
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
         <!-- bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/site/css/main-style.css">
