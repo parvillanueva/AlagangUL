228ee97d3bc6ef496e4ef61302ce384987f55bfb
King Villanueva <parvillanueva@gmail.com>
2020-02-05 09:42:04 +0800

    add all black page... update menu

---
+++ application/controllers/site/Programs.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Programs extends CI_Controller {
+
+	public function index()
+	{
+
+		$data['content'] = "site/programs/list";
+		$data['meta'] = array(
+			// "title"         =>  "Home",
+			// "description"   =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_description'),
+			// "keyword"       =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_keywords')
+		);
+		
+		$data['fb_og'] = array(
+			// "type"          =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_type'),
+			// "title"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_title'),
+			// "description"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_description'),
+			// "image"         =>  base_url().$this->Global_model->site_meta_og(38, 'site_menu', 'og_image'),
+		);
+		$data['active_menu'] = "programs";
+		$this->parser->parse("site/layout/template",$data);
+	}
+
+	public function view()
+	{
+
+		$data['content'] = "site/programs/view";
+		$data['meta'] = array(
+			// "title"         =>  "Home",
+			// "description"   =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_description'),
+			// "keyword"       =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_keywords')
+		);
+		
+		$data['fb_og'] = array(
+			// "type"          =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_type'),
+			// "title"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_title'),
+			// "description"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_description'),
+			// "image"         =>  base_url().$this->Global_model->site_meta_og(38, 'site_menu', 'og_image'),
+		);
+		$data['active_menu'] = "programs";
+		$this->parser->parse("site/layout/template",$data);
+	}
+
+}
