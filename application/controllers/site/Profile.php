58e51a8746128f915a2a8aa2d481bac36dfbf0aa
Gerald Martin Madarang <gmadarang@asi-dev4.com>
2020-02-06 10:41:12 +0800

    initial commit on profile module

---
+++ application/controllers/site/Profile.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller {
+
+	public function index()
+	{
+		//get basic details 
+		//get achievements
+		//get joined activities and events
+
+		$data['content'] = "site/profile/default";
+		$data['meta'] = array(
+			// "title"         =>  "Home",
+			// "description"   =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_description'),
+			// "keyword"       =>  $this->Global_model->site_meta_og(38, 'site_menu', 'meta_keywords')
+		);
+		
+		$data['fb_og'] = array(
+			// "type"          =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_type'),
+			// "title"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_title'),
+			// "description"         =>  $this->Global_model->site_meta_og(38, 'site_menu', 'og_description'),
+			// "image"         =>  base_url().$this->Global_model->site_meta_og(38, 'site_menu', 'og_image'),
+		);
+		
+		$this->parser->parse("site/layout/template",$data);
+	}
+
+}
