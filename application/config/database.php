60515f43d200ec4e0165667d8697f0ea109009b0
King Villanueva <parvillanueva@gmail.com>
2020-02-10 11:03:12 +0800

    update db connection

--- application/config/database.php
+++ application/config/database.php
@@ -1,31 +1,8 @@
 <?php defined("BASEPATH") OR exit("No direct script access allowed");
-		$whitelist = array(
-		    "127.0.0.1",
-		    "172.29.70.126",
-		    "::1"
-		);
-
-		if(in_array($_SERVER["REMOTE_ADDR"], $whitelist)){
-		    $db_host = "172.29.70.125";
-			$db_username = "phpdevsite";
-			$db_password = "unilab123";
-			$db_database = "teamapp_db";
-		}else{
-			$url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-			if (strpos($url,"webqa.unilab.com.ph") !== false) {
-				/* WEBQA Credentials */
-			    $db_host = preg_replace("(^https?://)", "", base_url() );
-				$db_username = "";
-				$db_password = "";
-				$db_database = "";
-			} else {
-				/* PRODUCTION Credentials */
-			    $db_host = preg_replace("(^https?://)", "", base_url() );
-				$db_username = "";
-				$db_password = "";
-				$db_database = "";
-			}
-		}
+		$db_host = "172.29.70.125";
+		$db_username = "phpdevsite";
+		$db_password = "unilab123";
+		$db_database = "teamapp_db";
 
 		$active_group = "default";
 		$query_builder = TRUE;
