c71a61d39bd2b4aef2575323e4a936832615c8b2
King Villanueva <parvillanueva@gmail.com>
2020-02-10 12:11:39 +0800

    fix profile

--- application/controllers/site/User_profile.php
+++ application/controllers/site/User_profile.php
@@ -1,7 +1,7 @@
 <?php
 defined("BASEPATH") OR exit("No direct script access allowed");
 
-class User extends GS_Controller {
+class User_profile extends GS_Controller {
 	public function index(){
 		//$data["title"] = "Content Management";
 		//$data["PageName"] = ("User");
