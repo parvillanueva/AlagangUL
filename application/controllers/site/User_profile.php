8a7cafe28a030793505472bdcdcc1aa862f4f18b
King Villanueva <parvillanueva@gmail.com>
2020-02-10 12:42:19 +0800

    fix controller

--- application/controllers/site/User_profile.php
+++ application/controllers/site/User_profile.php
@@ -1,7 +1,7 @@
 <?php
 defined("BASEPATH") OR exit("No direct script access allowed");
 
-class User_profile extends CI_Controller {
+class User extends CI_Controller {
 	public function index(){
 		//$data["title"] = "Content Management";
 		//$data["PageName"] = ("User");
