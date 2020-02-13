9f2beb72aa795476591c547934d80805959aa8e6
Jan <janjalanie@hotmail.com>
2020-02-13 09:33:22 +0800

    Added GTM script

--- application/views/site/layout/header.php
+++ application/views/site/layout/header.php
@@ -132,6 +132,15 @@
             }
 
         </style>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N9VJ329');
+        </script>
+        <!-- End Google Tag Manager -->
     </head>
     
     <p id="loading_div_standard"><i class="fa fa-spinner fa-spin" style="font-size:54px"></i></p>
