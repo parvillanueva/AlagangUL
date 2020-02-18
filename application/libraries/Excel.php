7939709d28cd2315fe862f5ef9188acd5a5c5c63
King Villanueva <parvillanueva@gmail.com>
2020-02-18 08:31:53 +0800

    fix phpexcel

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once dirname(__FILE__) . '/phpexcel/PHPExcel.php';
+require_once dirname(__FILE__) . '/PHPExcel/PHPExcel.php';
 
 class Excel extends PHPExcel {
     public function __construct() {
