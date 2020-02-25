7cbc70fe39c33fc2a08dbae67e496d2677a5f879
Jay-Marc <phpdeveloper9@unilab.com.ph>
2020-02-25 15:52:40 +0800

    cron job for workplace feeds

--- application/controllers/site/Cron.php
+++ application/controllers/site/Cron.php
@@ -5,42 +5,72 @@ class Cron extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('model');
 	}
 
 	public function index() {
 
-		$string = 'https://graph.facebook.com/507458846798175/feed?access_token=DQVJ1X3JxZAlRfM2pWN2I5eFVmVUJBYmhORENMSXM1bjZArbW4yOU13ZAmNYdFlqZA2hITWpQcnJEblg4UzB4bWYtV1BMcngxUE8xR2Q3SEI1WWk2bEdDX0toV0xFNVg5LXBnazV1Q1lmRHFNRHl1d1ZATeW9MaVMtdTBKckoyejQtX1lDTVRVc3poOWNTamx0d2RQRGtGeGtmVExRUDRTRi1ybl9Ub0liZAXlORU9VZAjVjaUlZAa1VvRDJMSWxQSUtkdjRWQ2xVWWNn&limit=100';
+		$string = 'https://graph.facebook.com/507458846798175/feed?access_token=DQVJ1X3JxZAlRfM2pWN2I5eFVmVUJBYmhORENMSXM1bjZArbW4yOU13ZAmNYdFlqZA2hITWpQcnJEblg4UzB4bWYtV1BMcngxUE8xR2Q3SEI1WWk2bEdDX0toV0xFNVg5LXBnazV1Q1lmRHFNRHl1d1ZATeW9MaVMtdTBKckoyejQtX1lDTVRVc3poOWNTamx0d2RQRGtGeGtmVExRUDRTRi1ybl9Ub0liZAXlORU9VZAjVjaUlZAa1VvRDJMSWxQSUtkdjRWQ2xVWWNn&limit=2';
 		$data = $this->curlExecute($string);
-		//echo "<pre>";
 		foreach ($data->data as $key => $value) {
 			$id = explode('_', $value->id);
 			$id = $id[1];
 			if(isset($value->message)){
 				
 				$string = 'https://graph.facebook.com/'.$id.'?fields=from,permalink_url,likes,attachments,created_time,updated_time&access_token=DQVJ1X3JxZAlRfM2pWN2I5eFVmVUJBYmhORENMSXM1bjZArbW4yOU13ZAmNYdFlqZA2hITWpQcnJEblg4UzB4bWYtV1BMcngxUE8xR2Q3SEI1WWk2bEdDX0toV0xFNVg5LXBnazV1Q1lmRHFNRHl1d1ZATeW9MaVMtdTBKckoyejQtX1lDTVRVc3poOWNTamx0d2RQRGtGeGtmVExRUDRTRi1ybl9Ub0liZAXlORU9VZAjVjaUlZAa1VvRDJMSWxQSUtkdjRWQ2xVWWNn';
-				$data = $this->curlExecute($string);
-				$date_posted = date('Y-m-d H:i:s',strtotime($data->created_time));
-				$link = 'https://unilab.facebook.com/groups/325111411161147';
-				if(isset($data->permalink_url)){
-					$link = $data->permalink_url;
-				}
+				$data2 = $this->curlExecute($string);
+				$likes_count = '';
 
+				if(isset($data2->likes)){
+                    $likes_count = count($data2->likes->data);
+                }
+                $link = 'https://unilab.facebook.com/groups/507458846798175';
+				if(isset($data->permalink_url)){
+					$link = $data2->permalink_url;
+				}
+				$user_fb_id = $data2->from->id;
+				$post_attachment = '';
 				$data_array = array(
-					'post_id' => $id,
-					'title' => $value->message,
-					'date_posted' => $date_posted,
-					'post_link' => $link,
-					'generated_date' => date('Y-m-d'),
-					'type' => 3
-					);
-				//print_r($data);
-				$checkifExisit = $this->model->checkifExisit('workplace_posts','post_id = '.$id);
-				if($checkifExisit[0]->count == 0){
-					$this->model->insert_data('workplace_posts',$data_array);
+					'post_id' 		=>	$id,
+					'post_by'		=>	$data2->from->name,
+					'date_posted' 	=>  date('Y-m-d H:i:s',strtotime($data2->created_time)),
+					'post_message'	=>  $value->message,
+					'likes'			=>	$likes_count,
+					'post_link'		=>	$link,
+					'generated_date'=>	date('Y-m-d H:i:s')
+				);
+
+
+
+				if(isset($data2->attachments)){
+					if(!isset($data2->attachments->data[0]->subattachments)){
+						if(isset($data2->attachments->data[0]->media)){
+                            $post_image = $data2->attachments->data[0]->media->image->src;
+                        }   
+					}
+					else{
+						if(isset($data2->attachments->data[0]->subattachments->data[0]->media)){
+                            $post_image = $data2->attachments->data[0]->subattachments->data[0]->subattachments->data[0]->media->image->src;
+                        }
+                        else{
+                            $post_image = $data2->attachments->data[0]->subattachments->data[0]->media->image->src;
+
+                        }      
+					}
+				}
+				$data_array['post_image']= $post_image;
+
+				$string = 'https://graph.facebook.com/'.$user_fb_id.'?fields=picture&access_token=DQVJ1X3JxZAlRfM2pWN2I5eFVmVUJBYmhORENMSXM1bjZArbW4yOU13ZAmNYdFlqZA2hITWpQcnJEblg4UzB4bWYtV1BMcngxUE8xR2Q3SEI1WWk2bEdDX0toV0xFNVg5LXBnazV1Q1lmRHFNRHl1d1ZATeW9MaVMtdTBKckoyejQtX1lDTVRVc3poOWNTamx0d2RQRGtGeGtmVExRUDRTRi1ybl9Ub0liZAXlORU9VZAjVjaUlZAa1VvRDJMSWxQSUtkdjRWQ2xVWWNn';
+				$data3 = $this->curlExecute($string);
+
+				$user_img = $data3->picture->data->url;
+				$data_array['post_by_img']= $data3->picture->data->url;
+
+				$checkifExisit = $this->Gmodel->get_query('tbl_workplace_feed','post_id = '.$id);
+				if(count($checkifExisit)== 0){
+					$this->Gmodel->save_data('tbl_workplace_feed',$data_array);
 				}
 				else{
-					$this->model->update_data('workplace_posts',$data_array,"post_id = ".$id);
+					$this->Gmodel->update_data('tbl_workplace_feed',$data_array,"post_id", $id);
 				}
 			}
 			
